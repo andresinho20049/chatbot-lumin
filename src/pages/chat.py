cf0904d819390c3d66c3dc118dc6cc697c9b91ce
andresinho20049 <andre.andresinho2009@hotmail.com>
2024-12-08 21:14:13 -0300

    feat: new page, chatBot.
    * langchain.ollama with baseUrl to local model
    * langchain (AiMessage, HumanMessage)
    * get_response with chat_history

--- src/pages/chat.py
+++ src/pages/chat.py
@@ -1,3 +1,65 @@
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+# from langchain_ollama.llms import OllamaLLM
+from langchain_ollama.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
-st.write("ChatBot Page!")
+st.header("_Streamlit_ is :blue[ChatBot Lumin] :robot_face:")
+
+def get_response(user_query, chat_history):
+
+    template = """
+    You are a helpful assistant. Answer the following questions considering the history of the conversation:
+
+    Chat history: {chat_history}
+
+    User question: {user_question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    llm = ChatOllama(
+        model = "andresinho20049/lumin",
+        base_url = "http://ollama:11434",
+        temperature = 0.8,
+        num_predict = 256
+    )
+        
+    chain = prompt | llm | StrOutputParser()
+    
+    # return llm.invoke(user_query)
+
+    return chain.stream({
+        "chat_history": chat_history,
+        "user_question": user_query,
+    })
+
+# session state
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I am a Lumin Bot. How can I help you?"),
+    ]
+
+    
+# conversation
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.write(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.write(message.content)
+
+# user input
+user_query = st.chat_input("Type your message here...")
+if user_query is not None and user_query != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+    with st.chat_message("Human"):
+        st.markdown(user_query)
+
+    with st.chat_message("AI"):
+        response = st.write_stream(get_response(user_query, st.session_state.chat_history))
+
+    st.session_state.chat_history.append(AIMessage(content=response))
